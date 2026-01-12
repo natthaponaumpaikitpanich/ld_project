68b4fac0a2d38a7394913cdf89922d9fbc114ca4
16 Hr <natthaponaumpaikitpanich@gmail.com>
2026-01-12 19:51:40 +0700

    google

--- loginpage/google_callback.php
+++ loginpage/google_callback.php
@@ -4,8 +4,8 @@ require_once "../ld_db.php";
 require_once "../vendor/autoload.php";
 
 $client = new Google\Client();
-$client->setClientId('837743881206-a4uglohivo68orpn5h88a9nhko7aga2g.apps.googleusercontent.com');
-$client->setClientSecret('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');
+// $client->setClientId('837743881206-a4uglohivo68orpn5h88a9nhko7aga2g.apps.googleusercontent.com');
+// $client->setClientSecret('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');
 $client->setRedirectUri('http://localhost/ld_project/loginpage/google_callback.php');
 
 if (!isset($_GET['code'])) {
--- loginpage/google_login.php
+++ loginpage/google_login.php
@@ -2,8 +2,8 @@
 require_once '../vendor/autoload.php';
 
 $client = new Google\Client();
-$client->setClientId('837743881206-a4uglohivo68orpn5h88a9nhko7aga2g.apps.googleusercontent.com');
-$client->setClientSecret('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');
+// $client->setClientId('837743881206-a4uglohivo68orpn5h88a9nhko7aga2g.apps.googleusercontent.com');
+// $client->setClientSecret('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');
 $client->setRedirectUri('http://localhost/ld_project/loginpage/google_callback.php');
 $client->addScope('email');
 $client->addScope('profile');
